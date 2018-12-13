70eeca63da65caec26ef3e9c606e3537b187788b
Lucas <lucashsmoraes@gmail.com>
2018-12-13 16:19:41 -0200

    como usar a prop 'key'

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 'use strict'
 import React, { Component } from 'react'
-import Title from './title'
+import Square from './square'
 
 class App extends Component{
   render() {
     return (
-      <div className='container' data-id='1'> 
-       <Title name='Lucas'/>
+      <div className='container' data-id='1'>
+      {['blue', 'red', 'green'].map((square) => (
+          <Square key={square} color={square}/>
+      ))}       
      </div>
     )
   }
