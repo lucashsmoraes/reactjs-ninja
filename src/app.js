a176068e67a7bd11bc4ddaa0aded328c819d1f92
Lucas Moraes <lucashsmoraes@gmail.com>
2018-12-14 00:00:22 -0200

    usando prop children

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 'use strict'
 import React, { Component } from 'react'
-import Square from './square'
+import Button from './button'
 
 class App extends Component {
   render () {
     return (
-      <div className='container' onClick={ (e) => {
-        alert('clicou')
-      }}>
-        <Square />
+      <div className='container'>
+        <Button>Texto</Button>
       </div>
     )
   }
