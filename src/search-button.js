2be3ec18559f13ac9e0d4ecc2ddc062e4c22d8a2
Lucas <lucashsmoraes@gmail.com>
2018-12-14 10:16:49 -0200

    Composição: fazendo que um componente tome formas diferentes

---
+++ src/search-button.js
@@ -0,0 +1,12 @@
+'use stric'
+
+import React from 'react'
+import Button from './button'
+
+const SearchButton = () => (
+  <Button handleClick={() => alert('SearchButton')}>
+    Search
+  </Button>
+)
+
+export default SearchButton
