b2d4b438ba12a02439062164f59f1b24814f596b
Lucas Moraes <lucashsmoraes@gmail.com>
2018-12-14 00:29:18 -0200

    entendo como usar composição

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 'use strict'
 import React, { Component } from 'react'
-import Button from './button'
+import LikeButton from './like-button'
+import SearchButton from './search-button'
 
 class App extends Component {
   render () {
     return (
       <div className='container'>
-        <Button>Texto</Button>
+        <LikeButton />
+        <SearchButton />
       </div>
     )
   }
