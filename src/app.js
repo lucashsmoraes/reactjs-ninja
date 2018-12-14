503f9276fdf071248143fd9d728500a2feff347a
Lucas <lucashsmoraes@gmail.com>
2018-12-14 11:55:15 -0200

    entendendo como funciona stateful e stateless

--- src/app.js
+++ src/app.js
@@ -1,21 +1,26 @@
 'use strict'
 import React, { Component } from 'react'
-import LikeButton from './like-button'
-import SearchButton from './search-button'
+import Button from './button'
+import Square from './square'
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
-      text: 'Lucas'
+        color: 'green'      
     }
   }
   render () {
     return (
-      <div className='container' onClick={() => this.setState({
-        text: 'Outro texto'
-      })}>
-        {this.state.text}
+      <div>
+        <Square color={this.state.color}/>
+        {['red', 'green', 'blue'].map((color) => (
+          <Button 
+            key={color} 
+            handleClick={() => this.setState({color})}>
+            {color}
+          </Button>
+        ))}
       </div>
     )
   }
