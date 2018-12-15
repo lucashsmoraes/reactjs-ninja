a3523d41d014bcb43e99b2cf015a760b5622b650
Lucas Moraes <lucashsmoraes@gmail.com>
2018-12-14 22:53:29 -0200

    exemplo de componenteDidMount e Unmount

--- src/app.js
+++ src/app.js
@@ -1,26 +1,21 @@
 'use strict'
+
 import React, { Component } from 'react'
-import Button from './button'
-import Square from './square'
+import Timer from './timer'
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
-        color: 'green'      
+      showTimer: true
     }
   }
   render () {
     return (
       <div>
-        <Square color={this.state.color}/>
-        {['red', 'green', 'blue'].map((color) => (
-          <Button 
-            key={color} 
-            handleClick={() => this.setState({color})}>
-            {color}
-          </Button>
-        ))}
+        {this.state.showTimer && <Timer />}
+        <button onClick={() =>
+          this.setState({ showTimer: !this.state.showTimer })}>Show / hide timer</button>
       </div>
     )
   }
