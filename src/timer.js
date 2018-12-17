7c54cec70f28c4bc783b62e570592c2ceef3f251
Lucas Moraes <lucashsmoraes@gmail.com>
2018-12-17 00:29:10 -0200

    LifeCycle: fluxo de atualização shouldComponentUpdate

--- src/timer.js
+++ src/timer.js
@@ -14,6 +14,11 @@ class Timer extends Component {
     console.log('componentWillReceiveProps', this.props, nextProps)
   }
 
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('shouldComponentUpdate', this.state, nextState)
+    return this.state.time !== nextState.time
+  }
+
   componentDidMount () {
     console.log('componentDidMount')
     this.timer = setInterval(() => this.setState({ time: this.state.time + 1 }), 1000)
