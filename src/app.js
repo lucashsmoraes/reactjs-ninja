43c4c5b16a9f2dc8e4b00c04d9240a9aafd2dc50
Lucas Moraes <lucashsmoraes@gmail.com>
2018-12-19 16:29:34 -0200

    marcação do projeto github app

--- src/app.js
+++ src/app.js
@@ -12,22 +12,41 @@ class App extends Component {
   }
   render () {
     return (
-      <div>
-        <label>
-          <input type='checkbox'
-            checked={this.state.checked} onChange={(e) => {
-              this.setState({
-                checked: !this.state.checked
-              }, () => {
-                this.setState({ showContent: this.state.checked })
-              })
-              // this.setState({
-              //   showContent: !this.state.checked
-              // })
-            }} />
-            Mostrar conteúdo
-        </label>
-        {this.state.showContent && <div>Olha eu aqui!</div>}
+      <div className='app'>
+        <div className='search'>
+          <input type='search' placeholder='Digite o nome do usuário' />
+        </div>
+        <div className='user-info'>
+          <img src='https://avatars1.githubusercontent.com/u/32342356?v=4' />
+          <h1>
+            <a href='https://github.com/lucashsmoraes'>Lucas Henrique Santana de Moraes</a>
+          </h1>
+
+          <ul className='repos-info'>
+            <li> Repositórios: 7</li>
+            <li> Seguidores: 1</li>
+            <li> Seguindo: 3</li>
+          </ul>
+
+          <div className='actions'>
+            <button>Ver Repositórios</button>
+            <button>Ver Favoritos</button>
+          </div>
+
+          <div className='repos'>
+            <h2>Repositórios: </h2>
+            <ul>
+              <li><a href='#'>Nome do repositório</a></li>
+            </ul>
+          </div>
+
+          <div className='starred'>
+            <h2>Favoritos: </h2>
+            <ul>
+              <li><a href='#'>Nome do repositório</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
