fb5b66504b8027df69bed3ff0d717b084f329585
Lucas Moraes <lucashsmoraes@gmail.com>
2018-12-20 19:17:04 -0200

    criando o objeto de estado da aplicação

--- src/app.js
+++ src/app.js
@@ -7,10 +7,27 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
+      userInfo: {
+        username: 'Lucas Moraes',
+        repos: 7,
+        followers: 1,
+        following: 3
+      },
+      repos: [{
+        name: 'Repo',
+        link: '#'
+      }],
+      starred: [{
+        name: 'Repo',
+        link: '#'
+      }]
     }
   }
   render () {
     return <AppContent
+      userInfo={this.state.userInfo}
+      repos={this.state.repos}
+      starred={this.state.starred}
     />
   }
 }
