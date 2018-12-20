865ff43d9b1d4b82665cba15f5893d2fa045bdb5
Lucas Moraes <lucashsmoraes@gmail.com>
2018-12-20 15:19:43 -0200

    Separando componentes e usando PropTypes(depreciado)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Search from './components/search'
+import UserInfo from './components/user-info'
+import Actions from './components/actions'
+import Repos from './components/repos'
 
 class App extends Component {
   constructor () {
@@ -13,40 +17,26 @@ class App extends Component {
   render () {
     return (
       <div className='app'>
-        <div className='search'>
-          <input type='search' placeholder='Digite o nome do usuário' />
-        </div>
-        <div className='user-info'>
-          <img src='https://avatars1.githubusercontent.com/u/32342356?v=4' />
-          <h1>
-            <a href='https://github.com/lucashsmoraes'>Lucas Henrique Santana de Moraes</a>
-          </h1>
 
-          <ul className='repos-info'>
-            <li> Repositórios: 7</li>
-            <li> Seguidores: 1</li>
-            <li> Seguindo: 3</li>
-          </ul>
-
-          <div className='actions'>
-            <button>Ver Repositórios</button>
-            <button>Ver Favoritos</button>
-          </div>
-
-          <div className='repos'>
-            <h2>Repositórios: </h2>
-            <ul>
-              <li><a href='#'>Nome do repositório</a></li>
-            </ul>
-          </div>
-
-          <div className='starred'>
-            <h2>Favoritos: </h2>
-            <ul>
-              <li><a href='#'>Nome do repositório</a></li>
-            </ul>
-          </div>
-        </div>
+        <Search />
+        <UserInfo />
+        <Actions />
+        <Repos
+          className='repos'
+          title='Repositórios:'
+          repos={[{
+            name: 'Nome do Repositório',
+            link: '#'
+          }]}
+        />
+        <Repos
+          className='starred'
+          title='Favoritos:'
+          repos={[{
+            name: 'Nome do Repositório',
+            link: '#'
+          }]}
+        />
       </div>
     )
   }
