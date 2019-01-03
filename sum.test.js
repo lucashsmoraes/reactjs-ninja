3534b124a94356d504878078fd6dd4546b3d993e
Lucas <lucashsmoraes@gmail.com>
2019-01-03 09:51:27 -0200

    code coverage

--- sum.test.js
+++ sum.test.js
@@ -1,10 +1,8 @@
 'use strict'
 
+const expect = require('chai').expect
+const sum = require('./sum')
 
-it('testando jest', () => {
-    expect(1).toBe(2)
-})
-
-it('um é igual a 1', () => {
-    console.assert(1 === 2, '1 não é igual a 2')
+it('sum should be a function', () => {
+    expect(sum).to.be.a('function')
 })
