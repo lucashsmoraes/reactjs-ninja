c230cadad50854955f9eb8cea2e506aa916568a9
Lucas <lucashsmoraes@gmail.com>
2019-01-03 14:52:15 -0200

    testando método map

--- sum.js
+++
@@ -1,5 +0,0 @@
-export default () => {
-    if(1 == 2){
-        return
-    }
-}
--- sum.test.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-import { expect } from 'chai'
-import sum from './sum'
-
-it('sum should be a function', () => {
-    expect(sum).to.be.a('function')
-})
