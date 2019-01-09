717e4345ea8c7ff865f6fe70355788ca8b14f95e
Lucas <lucashsmoraes@gmail.com>
2019-01-09 13:32:08 -0200

    configurando webpack-dashboard

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -6,27 +6,31 @@ const validate = require('webpack-validator')
 
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const DashboardPlugin = require('webpack-dashboard/plugin')
 
 module.exports = validate({
   devtool: 'source-map',
 
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, 'src', 'index')
-  ],
+  entry: path.join(__dirname, 'src', 'index'),
 
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name]-[hash].js',
-    publicPath: ''
+    filename: '[name]-[hash].js'
   },
 
   plugins: [
-    new DashboardPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    }),
+
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new HtmlPlugin({
       title: 'GitHub app',
       template: path.join(__dirname, 'src', 'html', 'template.html')
