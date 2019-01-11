3b163fdc1188c3c8f155ce61e9c4549bae2ca30c
Lucas <lucashsmoraes@gmail.com>
2019-01-11 14:20:47 -0200

    criado storia para o componente pagination e link

---
+++ app/src/components/pagination/pagination.story.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Pagination from './index';
+
+const stories = storiesOf('Pagination', module)
+    
+stories.add('without props', () => (
+    <Pagination />
+))
+
+stories.add('with props 5', () => (
+    <Pagination total={5} activePage={1} />
+))
+
+stories.add('with props 7', () => (
+    <Pagination total={7} activePage={5} />
+))
+
+stories.add('with props 15', () => (
+    <Pagination total={15} activePage={8} />
+))
