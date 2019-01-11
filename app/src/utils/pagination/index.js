aeefabdfaf0fbb91ae3f8ddcf89ad8f884c502e8
Lucas <lucashsmoraes@gmail.com>
2019-01-11 10:32:55 -0200

    testando o retorno quando 5 pages

--- app/src/utils/pagination/index.js
+++ app/src/utils/pagination/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const Pagination = ({total, activePage}) => {
-   return Array.from({length: total}, (_, i) => i + 1)
+   if(total <=5){
+      return Array.from({length: total}, (_, i) => i + 1)
+   }
+   return [1,2,3,'...',6]
 }
 
 export default Pagination
