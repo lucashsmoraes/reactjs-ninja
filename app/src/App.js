4b07000b61d9762225642350d04ae33e6c141780
Lucas <lucashsmoraes@gmail.com>
2019-01-16 12:19:47 -0200

    this.setState() usando funcoes p fazer att em lote

--- app/src/App.js
+++ app/src/App.js
@@ -3,31 +3,33 @@ import React, { Component } from 'react'
 class App extends Component {
   constructor () {
     super()
-    this.state = { title: '...',
-    Component : 'div'
+    this.state = { 
+      counter: 0
     }
   }
 
-  getTitle () {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve("My app with async / await!")
-      },2000)      
-    })
-  }
+  // getTitle () {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve("My app with async / await!")
+  //     },2000)      
+  //   })
+  // }
 
   async componentDidMount () {
-    const title = await import('./components/Title')
-    this.setState({
-      title: await this.getTitle(),
-      Component: title.default
-    })  
+    // const title = await import('./components/Title')
+    // this.setState({
+    //   title: await this.getTitle(),
+    //   Component: title.default
+    // })
+    this.setState((state) => ({counter: state.counter + 1}))  
   }
 
   render () {
+    const {counter} = this.state
     return (
       <div>
-        <this.state.Component>{this.state.title}</this.state.Component>
+        counter: {counter}
       </div>
     )
     
